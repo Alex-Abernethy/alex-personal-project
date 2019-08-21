fc288c33a3d39b416f9a09e8944ad1436e77a6e1
Alex-Abernethy <Alexander.Abernethy@gmail.com>
2019-08-21 20:21:55 +1200

    more setup

--- db.js
+++ db.js
@@ -3,14 +3,8 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
+  getTerrain: getTerrain,
 }
 
-function getUsers (db = connection) {
-  return db('users').select()
-}
-
-function getUser (id, db = connection) {
-  return db('users').where('id', id).first()
-}
+function getTerrain (db = connection)
+  return db('world').select
--- routes/home.js
+++ routes/home.js
@@ -6,15 +6,13 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   
-  // needs to get terrain instead
-
-  // db.getUsers()
-  //   .then(users => {
-  //     res.render('index', {users: users})
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send('DATABASE ERROR: ' + err.message)
-  //   })
+  db.getTerrain()
+  .then(users => {
+      res.render('index', {users: users})
+    })
+        .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 })
 
 module.exports = router
